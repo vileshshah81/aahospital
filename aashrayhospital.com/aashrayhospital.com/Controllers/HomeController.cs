395b6feae54f487f600bc956831c65c7f84ac74f
vilesh shah <vilesh.shah@techgrains.com>
2017-09-23 17:37:40 +0530

    maintance page added

--- aashrayhospital.com/aashrayhospital.com/Controllers/HomeController.cs
+++ aashrayhospital.com/aashrayhospital.com/Controllers/HomeController.cs
@@ -8,23 +8,28 @@ namespace aashrayhospital.com.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        public ActionResult UnderMaintenance()
         {
             return View();
         }
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
+        //public ActionResult Index()
+        //{
+        //    return View();
+        //}
 
-            return View();
-        }
+        //public ActionResult About()
+        //{
+        //    ViewBag.Message = "Your application description page.";
 
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
+        //    return View();
+        //}
 
-            return View();
-        }
+        //public ActionResult Contact()
+        //{
+        //    ViewBag.Message = "Your contact page.";
+
+        //    return View();
+        //}
     }
 }
