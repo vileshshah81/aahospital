55779c925f9c13301778ae7eeda9560ffdc06428
vilesh shah <vilesh.shah@techgrains.com>
2017-11-05 17:33:15 +0530

    changes

--- aashrayhospital.com/aashrayhospital.com/App_Start/RouteConfig.cs
+++ aashrayhospital.com/aashrayhospital.com/App_Start/RouteConfig.cs
@@ -23,6 +23,12 @@ namespace aashrayhospital.com
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "UnderMaintenance", id = UrlParameter.Optional }
             );
+
+            //routes.MapRoute(
+            //    name: "Default",
+            //    url: "{controller}/{action}/{id}",
+            //    defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            //);
         }
     }
 }
